d8d3f857c50eb9dfe66e221a24b516e8c5144aef
Cho-YH <xxxxoov@kakao.com>
2021-01-20 00:55:15 +0900

    환경세팅 완료

---
+++ src/test/java/com/cho/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cho.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
